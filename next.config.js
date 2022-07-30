ebece296b86598422be5dd5b30811d13deeb7051
geekdada <me@dada.li>
2022-07-30 15:11:52 +0200

    fix: style and image issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'zh-Hans'],
+    defaultLocale: 'en',
+  },
   images: {
     domains: ['resource-proxy.royli.dev'],
   },
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -36,117 +36,113 @@ const AdminHome: NextPage<Props> = ({ user }) => {
   }
 
   return (
-    <>
-      <div className="container mx-auto max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-        <h1 className="heading-text mb-8 text-4xl font-bold font-title">
-          Admin
-        </h1>
+    <div className="container mx-auto px-6 max-w-3xl lg:max-w-5xl xl:max-w-7xl">
+      <h1 className="heading-text mb-8 text-4xl font-bold font-title">Admin</h1>
 
-        <div className="bg-white dark:bg-dark-700 drop-shadow-md rounded overflow-hidden p-8 space-y-9">
-          <div className="space-y-4">
-            <h2 className="text-xl font-bold">Revalidate blog post list</h2>
-            <button
-              className="rounded-lg px-4 py-2 bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
-              onClick={async () => {
-                await ky.post('/api/admin/revalidate', {
-                  json: {
-                    type: 'blog-posts',
-                  },
-                })
-              }}
-            >
-              Revalidate
-            </button>
-          </div>
+      <div className="bg-white dark:bg-dark-700 drop-shadow-md rounded overflow-hidden p-8 space-y-9">
+        <div className="space-y-4">
+          <h2 className="text-xl font-bold">Revalidate blog post list</h2>
+          <button
+            className="rounded-lg px-4 py-2 bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
+            onClick={async () => {
+              await ky.post('/api/admin/revalidate', {
+                json: {
+                  type: 'blog-posts',
+                },
+              })
+            }}
+          >
+            Revalidate
+          </button>
+        </div>
 
-          <div className="space-y-4">
-            <h2 className="text-xl font-bold">Revalidate blog post</h2>
+        <div className="space-y-4">
+          <h2 className="text-xl font-bold">Revalidate blog post</h2>
 
-            <Formik
-              initialValues={{
-                URI: '',
-              }}
-              validationSchema={RevalidateBlogPostSchema}
-              onSubmit={async (values, actions) => {
-                await ky.post('/api/admin/revalidate', {
-                  json: {
-                    type: 'uri',
-                    uri: values.URI,
-                  },
-                })
-                actions.resetForm()
-              }}
-            >
-              {({ errors, touched, isSubmitting }) => (
-                <Form className="space-y-3">
-                  <div>
-                    <Field
-                      autoComplete="off"
-                      className="w-full border rounded px-4 py-2 primary-text"
-                      name="URI"
-                      placeholder="/blog/2022/..."
-                    />
-                  </div>
-                  {errors.URI && touched.URI ? (
-                    <div className="text-red-500">{errors.URI}</div>
-                  ) : null}
+          <Formik
+            initialValues={{
+              URI: '',
+            }}
+            validationSchema={RevalidateBlogPostSchema}
+            onSubmit={async (values, actions) => {
+              await ky.post('/api/admin/revalidate', {
+                json: {
+                  type: 'uri',
+                  uri: values.URI,
+                },
+              })
+              actions.resetForm()
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form className="space-y-3">
+                <div>
+                  <Field
+                    autoComplete="off"
+                    className="w-full border rounded px-4 py-2 primary-text"
+                    name="URI"
+                    placeholder="/blog/2022/..."
+                  />
+                </div>
+                {errors.URI && touched.URI ? (
+                  <div className="text-red-500">{errors.URI}</div>
+                ) : null}
 
-                  <button
-                    type="submit"
-                    className="rounded-lg px-4 py-2 bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
-                  >
-                    Revalidate
-                  </button>
-                </Form>
-              )}
-            </Formik>
-          </div>
+                <button
+                  type="submit"
+                  className="rounded-lg px-4 py-2 bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
+                >
+                  Revalidate
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
 
-          <div className="space-y-4">
-            <h2 className="text-xl font-bold">Revalidate page</h2>
+        <div className="space-y-4">
+          <h2 className="text-xl font-bold">Revalidate page</h2>
 
-            <Formik
-              initialValues={{
-                URI: '',
-              }}
-              validationSchema={RevalidatePageSchema}
-              onSubmit={async (values, actions) => {
-                await ky.post('/api/admin/revalidate', {
-                  json: {
-                    type: 'uri',
-                    uri: values.URI,
-                  },
-                })
-                actions.resetForm()
-              }}
-            >
-              {({ errors, touched, isSubmitting }) => (
-                <Form className="space-y-3">
-                  <div>
-                    <Field
-                      autoComplete="off"
-                      className="w-full border rounded px-4 py-2 primary-text"
-                      name="URI"
-                      placeholder="/page/..."
-                    />
-                  </div>
-                  {errors.URI && touched.URI ? (
-                    <div className="text-red-500">{errors.URI}</div>
-                  ) : null}
+          <Formik
+            initialValues={{
+              URI: '',
+            }}
+            validationSchema={RevalidatePageSchema}
+            onSubmit={async (values, actions) => {
+              await ky.post('/api/admin/revalidate', {
+                json: {
+                  type: 'uri',
+                  uri: values.URI,
+                },
+              })
+              actions.resetForm()
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form className="space-y-3">
+                <div>
+                  <Field
+                    autoComplete="off"
+                    className="w-full border rounded px-4 py-2 primary-text"
+                    name="URI"
+                    placeholder="/page/..."
+                  />
+                </div>
+                {errors.URI && touched.URI ? (
+                  <div className="text-red-500">{errors.URI}</div>
+                ) : null}
 
-                  <button
-                    type="submit"
-                    className="rounded-lg px-4 py-2 bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
-                  >
-                    Revalidate
-                  </button>
-                </Form>
-              )}
-            </Formik>
-          </div>
+                <button
+                  type="submit"
+                  className="rounded-lg px-4 py-2 bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
+                >
+                  Revalidate
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
