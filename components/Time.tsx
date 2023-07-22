0c3824aac9b66505ab68715a9262d56966222aa4
Roy Li <me@royli.dev>
2023-07-23 01:09:21 +0200

    fix: time hydration issue

--- components/Time.tsx
+++ components/Time.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 
 dayjs.extend(localizedFormat)
 
 const Time = ({ datetime, format }: { datetime: string; format?: string }) => {
-  return <span>{dayjs(datetime).format(format || 'MMM D, YYYY h:mm A')}</span>
+  const [formattedDateTime, setFormattedDateTime] = useState<null | string>(
+    null
+  )
+
+  useEffect(() => {
+    if (formattedDateTime === null) {
+      setFormattedDateTime(dayjs(datetime).format(format || 'lll'))
+    }
+  }, [formattedDateTime, datetime, format])
+
+  return formattedDateTime ? (
+    <time dateTime={datetime}>{formattedDateTime}</time>
+  ) : (
+    <time dateTime={datetime}>{datetime}</time>
+  )
 }
 
 export default Time
