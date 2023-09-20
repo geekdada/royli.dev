9964c6ce50f9cf2ff2a6a7f2287bf8f806c8c418
Roy Li <me@royli.dev>
2023-09-21 00:29:53 +0200

    update link

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,6 +67,15 @@ const Home: NextPage<Props> = ({ posts }) => {
                 className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-indigo-500 text-white hover:bg-indigo-400/80"
               >
                 Mastodon
+              </a>
+              {', '}
+              <a
+                href="https://unsplash.com/@geekdada"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-dark-900 text-white hover:bg-dark-900/80"
+              >
+                Unsplash
               </a>{' '}
               and{' '}
               <a
