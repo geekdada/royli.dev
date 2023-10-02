077e3743ac764bc39ff91360e1b0aea119bb82f0
Roy Li <me@royli.dev>
2023-10-02 21:50:11 +0200

    fix: edge function

--- pages/api/tools/shortlink.ts
+++ pages/api/tools/shortlink.ts
@@ -5,37 +5,38 @@ import {
 } from 'next/server'
 import { createEdgeRouter } from 'next-connect'
 import boom from '@hapi/boom'
-import * as Yup from 'yup'
 
 const router = createEdgeRouter<NextRequest, NextFetchEvent>()
-const schema = Yup.object().shape({
-  destination: Yup.string().required(),
-  slashtag: Yup.string(),
-  title: Yup.string(),
-  fullname: Yup.string(),
-  apikey: Yup.string().required(),
-})
 
 export const config = {
   runtime: 'edge',
 }
 
 router.post(async (req) => {
-  const body = await req.json()
-  const payload = await schema.validate(body)
+  const body = (await req.json()) as {
+    destination?: string
+    slashtag?: string
+    title?: string
+    fullname?: string
+    apikey?: string
+  }
+
+  if (!body.destination) throw boom.badRequest('Missing destination')
+  if (!body.apikey) throw boom.badRequest('Missing apikey')
+
   const response = await fetch('https://api.rebrandly.com/v1/links', {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
       accept: 'application/json',
-      apikey: payload.apikey,
+      apikey: body.apikey,
     },
     body: JSON.stringify({
-      destination: payload.destination,
-      slashtag: payload.slashtag,
-      title: payload.title,
+      destination: body.destination,
+      slashtag: body.slashtag,
+      title: body.title,
       domain: {
-        fullName: payload.fullname || 'url.royli.dev',
+        fullName: body.fullname || 'url.royli.dev',
       },
     }),
   }).then(
@@ -57,10 +58,6 @@ export default router.handler({
     if (boom.isBoom(err)) {
       return new NextResponse(err.message, {
         status: err.output.statusCode,
-      })
-    } else if (err instanceof Yup.ValidationError) {
-      return new NextResponse(err.message, {
-        status: 400,
       })
     } else if (err instanceof Error) {
       return new NextResponse(err.message, {
