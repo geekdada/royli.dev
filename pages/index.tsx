421dbe42fa4f69c5c066e1792e2f5bb2ec8332b0
Roy Li <me@royli.dev>
2024-11-21 21:40:27 +0100

    Refactor button styles for improved readability and consistency

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ export default function IndexPage({
                 href="https://github.com/geekdada"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-dark-600 text-white hover:bg-dark-600/80"
+                className="mx-1 py-1 px-2 text-sm md:text-base rounded font-semibold transition-all duration-150 bg-dark-600 text-white hover:bg-dark-600/80"
               >
                 GitHub
               </a>
@@ -91,7 +91,7 @@ export default function IndexPage({
                 href="https://twitter.com/geekdada"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-blue-400 text-white hover:bg-blue-400/80"
+                className="mx-1 py-1 px-2 text-sm md:text-base rounded font-semibold transition-all duration-150 bg-blue-400 text-white hover:bg-blue-400/80"
               >
                 Twitter
               </a>
@@ -99,7 +99,7 @@ export default function IndexPage({
               <a
                 href="https://tooted.space/@geekdada"
                 rel="me"
-                className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-indigo-500 text-white hover:bg-indigo-400/80"
+                className="mx-1 py-1 px-2 text-sm md:text-base rounded font-semibold transition-all duration-150 bg-indigo-500 text-white hover:bg-indigo-400/80"
               >
                 Mastodon
               </a>
@@ -108,7 +108,7 @@ export default function IndexPage({
                 href="https://unsplash.com/@geekdada"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-dark-900 text-white hover:bg-dark-900/80"
+                className="mx-1 py-1 px-2 text-sm md:text-base rounded font-semibold transition-all duration-150 bg-dark-900 text-white hover:bg-dark-900/80"
               >
                 Unsplash
               </a>
@@ -117,7 +117,7 @@ export default function IndexPage({
                 href="https://www.linkedin.com/in/roy-li-gz/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="mx-1 py-1 px-2 rounded font-bold transition-all duration-150 bg-blue-600 text-white hover:bg-blue-600/80"
+                className="mx-1 py-1 px-2 text-sm md:text-base rounded font-semibold transition-all duration-150 bg-blue-600 text-white hover:bg-blue-600/80"
               >
                 LinkedIn
               </a>
